3a403002cb5f0445d3fe24eb4848ef71b530c2a3
Annisa Fitriatuzzahra <annisa.fitriatuzzahra@gmail.com>
2022-10-15 15:32:19 +0700

    refactor: rebase from main

---
+++ src/pages/profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Head from 'next/head';
+
+const ProfilePage = () => (
+  <>
+    <Head>
+      <title>Moviku</title>
+    </Head>
+
+    <div>ProfilePage</div>
+  </>
+);
+
+export default ProfilePage;
