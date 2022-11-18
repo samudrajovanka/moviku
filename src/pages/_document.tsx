293db4e24648d053ebf9f525175708d52c027137
Jovanka Samudra <samudrajovanka20@gmail.com>
2022-11-18 19:31:04 +0700

    style: tidy up code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,24 +32,3 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
-
-// export default function Document() {
-//   return (
-//     <Html lang="en">
-//       <Head>
-//         <link rel="icon" href="/favicon.ico" />
-
-//         <link rel="preconnect" href="https://fonts.googleapis.com" />
-//         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-//         <link
-//           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
-//           rel="stylesheet"
-//         />
-//       </Head>
-//       <body>
-//         <Main />
-//         <NextScript />
-//       </body>
-//     </Html>
-//   );
-// }
